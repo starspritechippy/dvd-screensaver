c011ef642a897d4748951843db07f09b7f793a83
Chippy <dev@chippy.wtf>
2023-08-15 18:49:45 +0200

    it works

--- classes/sprite.py
+++ classes/sprite.py
@@ -39,6 +39,36 @@ class Sprite:
             raise ValueError(
                 "Needs either a size or a scale, you have provided none of those two."
             )
+        color_image = pygame.Surface(self.image.get_size()).convert_alpha()
+        color_image.fill
+
+    @property
+    def pos_x_end(self) -> float:
+        return self.pos_x + self.width
+
+    @property
+    def pos_y_end(self) -> float:
+        return self.pos_y + self.height
+
+    @property
+    def bounding_box(self) -> pygame.Rect:
+        return pygame.Rect(self.pos_x, self.pos_y, self.width, self.height)
+
+    def simulate_move(self, movement: pygame.Vector2) -> pygame.Rect:
+        """Returns True if movement was possible and False if not."""
+        simulated_rect = self.bounding_box
+        simulated_rect.x += movement.x
+        simulated_rect.y += movement.y
+        return simulated_rect
+
+    def move(self, movement: pygame.Vector2, borders: pygame.Rect) -> bool:
+        """Returns True if movement was possible and False if not."""
+        simulated_rect = self.simulate_move(movement)
+        if borders.contains(simulated_rect):
+            self.pos_x += movement.x
+            self.pos_y += movement.y
+            return True
+        return False
 
     def draw(self, window: Surface):
         window.blit(self.image, (self.pos_x, self.pos_y))
--- main.py
+++ main.py
@@ -2,18 +2,37 @@
 import pygame
 from classes.sprite import Sprite
 
+
+class Line:
+    point_a: tuple[float, float]
+    point_b: tuple[float, float]
+
+    def __init__(
+        self, point_a: tuple[float, float], point_b: tuple[float, float]
+    ) -> None:
+        self.point_a = point_a
+        self.point_b = point_b
+
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+screen_rect = screen.get_rect()
 clock = pygame.time.Clock()
 running = True
 
-dvd_image = pygame.image.load("assets/dvd-cropped.png").convert_alpha()
-dvd_image_resized = pygame.transform.smoothscale_by(dvd_image, 0.1)
-
 dvd_sprite = Sprite("assets/dvd-cropped.png", (400, 400), scale_factor=0.1)
+dvd_movement = pygame.Vector2(2, -2)  # .normalize()
 
 while running:
+    screen_rect = screen.get_rect()
+    screen_borders = (
+        Line(screen_rect.topleft, screen_rect.topright),
+        Line(screen_rect.bottomleft, screen_rect.bottomright),
+        Line(screen_rect.topright, screen_rect.bottomright),
+        Line(screen_rect.topleft, screen_rect.bottomleft),
+    )
+
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
@@ -24,7 +43,31 @@ while running:
     screen.fill("white")
 
     # RENDER YOUR GAME HERE
+    # spawn dvd logo
     dvd_sprite.draw(screen)
+
+    # now move it up right
+    if not dvd_sprite.move(dvd_movement, screen.get_rect()):
+        if dvd_sprite.simulate_move(dvd_movement).clipline(
+            screen_borders[0].point_a, screen_borders[0].point_b
+        ):
+            # top line clipped
+            dvd_movement = dvd_movement.reflect(pygame.Vector2(0, -1))
+        if dvd_sprite.simulate_move(dvd_movement).clipline(
+            screen_borders[1].point_a, screen_borders[1].point_b
+        ):
+            # bottom line clipped
+            dvd_movement = dvd_movement.reflect(pygame.Vector2(0, -1))
+        if dvd_sprite.simulate_move(dvd_movement).clipline(
+            screen_borders[2].point_a, screen_borders[2].point_b
+        ):
+            # right line clipped
+            dvd_movement = dvd_movement.reflect(pygame.Vector2(-1, 0))
+        if dvd_sprite.simulate_move(dvd_movement).clipline(
+            screen_borders[3].point_a, screen_borders[3].point_b
+        ):
+            # left line clipped
+            dvd_movement = dvd_movement.reflect(pygame.Vector2(-1, 0))
 
     # flip() the display to put your work on screen
     pygame.display.flip()
